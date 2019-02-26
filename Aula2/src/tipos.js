3203909d333d273b3a46c17de1c688a341d4ab1d
rmascota <47834707+rmascota@users.noreply.github.com>
2019-02-26 18:47:22 -0300

    Aula dia 26/02

---
+++ Aula2/src/tipos.js
@@ -0,0 +1,15 @@
+var booleano = true;
+var inteiro = 18;
+var pontoFlutuante = 10.2;
+var textoString = 'Oi, Eu sou o nicão';
+var semAtribuicaodeTipo;
+var nulo = null;
+
+console.log(`
+    \nBooleano: ${booleano}
+    \ninteiro: ${inteiro}
+    \nPonto Flutuante: ${pontoFLutuante}
+    \nString: ${textoString}
+    \nSem Atributo: ${semAtribuicaoDeTipo}
+    \nVariavel Nula: ${nulo}
+`);
